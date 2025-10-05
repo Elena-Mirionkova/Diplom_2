cac40c206a70f345f9c4ad090da826d7260416be
Elena Mirionkova <elena_mirionkova@yandex.ru>
2025-10-05 21:37:02 +0400

    release candidate v1.2

--- data.py
+++ data.py
@@ -1,14 +1,5 @@
 from generators import *
-
-class Url:
-    main_url = "https://stellarburgers.nomoreparties.site"
-    create_user = "/api/auth/register"
-    login_user = "/api/auth/login"
-    logout_user = "/api/auth/logout"
-    delete_user = "/api/auth/user"
-    create_order = "/api/orders"
-    get_orders =  "/api/orders"
-    
+   
 class ResponseCodes:
     created = 201
     server_error = 500
---
+++ urls.py
@@ -0,0 +1,8 @@
+class Url:
+    main_url = "https://stellarburgers.nomoreparties.site"
+    create_user = "/api/auth/register"
+    login_user = "/api/auth/login"
+    logout_user = "/api/auth/logout"
+    delete_user = "/api/auth/user"
+    create_order = "/api/orders"
+    get_orders =  "/api/orders"
